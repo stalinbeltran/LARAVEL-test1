06456faa87dc6f8dc15f4dc7bdf1450372cbd8cc
Stalin Beltran <stalin.beltran2006@gmail.com>
2021-08-19 17:48:13 -0500

    mostramos welcome1 en lugar de welcome. No se muestra login

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome1');
+});
+Route::get('/login', function () {
+    return view('login');
 });
