1c02b87cf5ca8479642f6d1cf968b01253f6f65d
Stalin Beltran <stalin.beltran2006@gmail.com>
2021-08-20 15:58:16 -0500

    corregido, muestra estudiantes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudInsertController;
+use App\Http\Controllers\StudViewController;
 
 /*
 |--------------------------------------------------------------------------
